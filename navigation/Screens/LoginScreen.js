d47fc23d00c72f09a250c59ab22908988f8d790d
phucbndx <phucnguyendx9012@gmail.com>
2023-10-08 00:22:29 +0700

    update login screen

--- navigation/Screens/LoginScreen.js
+++ navigation/Screens/LoginScreen.js
@@ -1,12 +1,81 @@
-import React from 'react'
-import { View,Text } from 'react-native'
+import React, { useState } from 'react';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image
+} from 'react-native';
+
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+import LoginSVG from '../../assets/Images/login.png'
+import InputField from '../../component/InputField';
+import CustomButton from '../../component/CustomButton';
+import { useNavigation } from '@react-navigation/native';
 
 export default function LoginScreen({setIsLogin}) {
+  const navigation = useNavigation();
+const [showPassWord,setShowPassWord]=useState(false)
   return (
-    <View>
-        <Text style={{marginTop:50}} onPress={()=>{setIsLogin(true)}}>
-        LoginScreen
+    <SafeAreaView style={{flex: 1, justifyContent: 'center'}}>
+      <View style={{paddingHorizontal: 25}}>
+        <View style={{alignItems: 'center'}}>
+      
+            <Image
+        source={LoginSVG} 
+        style={{ width: '100%', height: 180, transform: [{ rotate: '-5deg' }],marginBottom:20 }} 
+      />
+        </View>
+
+        <Text
+          style={{
+            fontSize: 28,
+            fontWeight: '500',
+            color: '#333',
+            marginBottom: 30,
+          }}>
+          Login
         </Text>
-    </View>
+
+        <InputField
+          label={'Email ID'}
+          icon={
+            <MaterialIcons
+            name="alternate-email"
+            size={20}
+            color="#666"
+            style={{marginRight: 5}}
+          />
+          }
+          keyboardType="email-address"
+        />
+
+<InputField
+          label={'Password'}
+          icon={
+            <Ionicons
+            name="ios-lock-closed-outline"
+            size={20}
+            color="#666"
+            style={{marginRight: 5}}
+          />
+          }
+          inputType={showPassWord?'':"password"}
+          fieldButtonLabel={showPassWord ? 'Hidden':'Show'}
+          fieldButtonFunction={() => {
+       setShowPassWord(!showPassWord);
+          }}
+        />
+        
+        <CustomButton label={"Login"} onPress={() => {
+          console.log('login ');
+        }} />
+
+
+      </View>
+    </SafeAreaView>
   )
 }
--- navigation/Screens/UserScreen.js
+++ navigation/Screens/UserScreen.js
@@ -1,17 +1,61 @@
-import React from "react";
-import { View, Text } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useEffect } from 'react';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import { useNavigation } from '@react-navigation/native';
 
 function UserScreen() {
+  const [hasPermission, setHasPermission] = React.useState(false);
+  const [scanData, setScanData] = React.useState();
+  const [shouldShowCamera, setShouldShowCamera] = React.useState(true);
+    const navigation = useNavigation();
+  useEffect(() => {
+    (async() => {
+      const {status} = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  if (!hasPermission) {
+    return (
+      <View style={styles.container}>
+        <Text>Please grant camera permissions to app.</Text>
+      </View>
+    );
+  }
+
+  const handleBarCodeScanned = ({type, data}) => {
+    setScanData(data);
+    console.log(`Data: ${data}`);
+    console.log(`Type: ${type}`);
+    setShouldShowCamera(false);
+    navigation.navigate('Home');
+  };
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text
-        onPress={() => navigation.navigate("Home")}
-        style={{ fontSize: 26, fontWeight: "bold" }}
-      >
-        UserScreen
-      </Text>
-    </View>
+    <View style={styles.container}>
+   {shouldShowCamera && (
+        <BarCodeScanner
+          style={StyleSheet.absoluteFillObject}
+          onBarCodeScanned={scanData ? undefined : handleBarCodeScanned}
+        />
+      )}
+      {scanData && <Button title='Scan Again?' onPress={() => {
+        setScanData(undefined)
+        setShouldShowCamera(true)
+      }
+      } />}
+      <StatusBar style="auto" />
+  </View>
   );
 }
-
 export default UserScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop:20
+  },
+});
